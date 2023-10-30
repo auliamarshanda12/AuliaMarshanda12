8fa8df95eca84599fd838c7db7e4c57ebe7932b9
auliamarshanda12 <148952831+auliamarshanda12@users.noreply.github.com>
2023-10-30 09:06:17 +0700

    Add files via upload

---
+++ Streamlit_Boston.py
@@ -0,0 +1,23 @@
+import pickle
+import numpy as np
+import streamlit as st
+
+model = pickle.load(open('PrediksiBoston3.sav', 'rb'))
+
+st.title('Prediksi Harga Rumah Boston')
+
+INDUS = st.number_input('input INDUS')
+CHAS = st.number_input('input CHAS')
+NOX = st.number_input('input NOX')
+RM = st.number_input('input RM')
+AGE = st.number_input('input AGE')
+TAX = st.number_input('input TAX')
+LSTAT = st.number_input('input LSTAT')
+
+    
+prediksi=''
+if st.button('Prediksi Peluang Masuk Pendidikan Tinggi'):
+    prediksi_boston = model.predict([[INDUS, CHAS,	NOX,	RM,	AGE,	TAX,	LSTAT]])
+    prediksi=prediksi_boston
+
+st.success(prediksi)
